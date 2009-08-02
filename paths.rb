206e003b885fdf25fdf8349042e05b165e0e322c
Mark Hansen <mark@markhansen.co.nz>
2009-08-02 15:49:21 +1200

    cleaned paths.rb

--- paths.rb
+++ paths.rb
@@ -1,11 +1,11 @@
 #converts cygwin paths (or path lists separated by :) to windows paths with forward slashes
 #e.g. /cygdrive/c/temp -> c:/temp
-def to_winPath(unixPath)
-  #make windows paths with forward slashes
-  cygpath = "cygpath -m" 
-
+def to_winPath(unixPath, escapeSlashes=true)
+  cygpath = "cygpath -m"
   isPathList = unixPath.include? ":"
-  if isPathList then cygpath += " -p" end 
+  if (isPathList) 
+      cygpath = "cygpath -m -p"
+  end
 
   #escape semicolons
   cp = IO.popen(cygpath + " " + unixPath).readline.chomp.gsub(/;/,"\\;")
